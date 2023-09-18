a9f60437c0e818e5633775c090a496a0c4534419
Elothamza <elotmanihamza61@gmail.com>
2023-09-18 03:15:03 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - writes the character str
+ * @str: The string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+		i++;
+	}
+	_putchar('\n');
+}
